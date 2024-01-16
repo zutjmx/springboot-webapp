8dca08887395ba3049138212faf9c1fc37d4c4a6
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-01-15 19:31:53 -0600

    Se agrega y se implementa la depedencia JavaFaker

--- src/main/java/com/zutjmx/curso/springboot/webapp/springbootwebapp/controllers/UsuarioRestController.java
+++ src/main/java/com/zutjmx/curso/springboot/webapp/springbootwebapp/controllers/UsuarioRestController.java
@@ -2,6 +2,8 @@ package com.zutjmx.curso.springboot.webapp.springbootwebapp.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.javafaker.Faker;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,5 +22,16 @@ public class UsuarioRestController {
         respuesta.put("materno", "Trejo");
         return respuesta;
     }
+
+    @GetMapping("/usuario")
+    public Map<String, Object> getUsuario() {
+        Map<String, Object> respuesta = new HashMap<>();
+        Faker faker = new Faker();
+        respuesta.put("useragent", faker.internet().userAgentAny());
+        respuesta.put("email", faker.internet().emailAddress());
+        respuesta.put("uuid", faker.internet().uuid());
+        return respuesta;
+    }
+    
     
 }
