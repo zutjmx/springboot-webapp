faa8fd546efd9e4a3a0fb2b6e1576741f40b4a4f
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-01-17 15:03:59 -0600

    Se agrega html con lista de usuarios

--- src/main/java/com/zutjmx/curso/springboot/webapp/springbootwebapp/controllers/UsuarioController.java
+++ src/main/java/com/zutjmx/curso/springboot/webapp/springbootwebapp/controllers/UsuarioController.java
@@ -1,10 +1,13 @@
 package com.zutjmx.curso.springboot.webapp.springbootwebapp.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.github.javafaker.Faker;
@@ -28,6 +31,27 @@ public class UsuarioController {
         usuarioDto.setSaludo("Hola Mundo con SpringBoot y ThymeLeaf");
         return usuarioDto;
     }
+
+    @GetMapping("/listar")
+    public String listar(ModelMap modelMap) {
+        List<Usuario> usuarios = new ArrayList<>();
+        Faker faker = new Faker(Locale.getDefault());
+        int indice = faker.number().numberBetween(10, 20);
+        //int indice = 0;
+        for (int i = 0; i < indice; i++) {
+            Usuario usuario = new Usuario(
+                faker.name().firstName(), 
+                faker.name().lastName(), 
+                faker.name().lastName(), 
+                faker.internet().emailAddress()
+                );
+            usuarios.add(usuario);
+        }
+        modelMap.addAttribute("titulo", "Listado de Usuarios");
+        modelMap.addAttribute("usuarios", usuarios);
+        return "listar";
+    }
+    
 
     @GetMapping("/detalles-modelo-map")
     public String detallesModeloMap(Model modelo, Map<String, Object> mapa) {
