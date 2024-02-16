1da29d74e73a8f9de5901e6f3740789b98349c7c
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-02-16 15:34:13 -0600

    Se agrega método mixPathVar a la clase PathVariableController

--- src/main/java/com/zutjmx/curso/springboot/webapp/springbootwebapp/controllers/PathVariableController.java
+++ src/main/java/com/zutjmx/curso/springboot/webapp/springbootwebapp/controllers/PathVariableController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.zutjmx.curso.springboot.webapp.springbootwebapp.models.dto.ParametroDto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -17,6 +20,14 @@ public class PathVariableController {
         ParametroDto parametroDto = new ParametroDto();
         parametroDto.setMensaje(mensaje);
         return parametroDto;
+    }
+
+    @GetMapping("/mix/{producto}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String producto, @PathVariable Long id) {
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("producto", producto);
+        jsonMap.put("id", id);
+        return jsonMap;
     }
     
 }
